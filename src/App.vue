ff348f4e26753b283cc8c118c70a8b4ca6a1eba6
Elechi George <elechigeorgepro@gmail.com>
2020-07-15 17:37:59 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <img width="25%" src="./assets/logo.png">
+    <Hero />
+  </div>
+</template>
+
+<script>
+import Hero from './components/hero-section'
+
+export default {
+  name: "App",
+  components: {
+    Hero
+  }
+};
+</script>
+
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+}
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+img {
+  margin: 0; padding: 0;
+  top: 10px;
+}
+</style>
---
+++ src/components/hero-section.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <h1>VueTodo</h1>
+    <p>The vue app for developers</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {}
+};
+</script>
+
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+}
+h1 {
+  color: #2c3e50;
+}
+</style>
